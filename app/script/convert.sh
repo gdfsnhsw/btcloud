9714c4fde7d8fb81004c39f5c28ffb6a27e7d5a0
net909 <net909@163.com>
2023-07-21 17:34:11 +0800

    update

--- app/script/convert.sh
+++ app/script/convert.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-Linux_Version="8.0.0"
-Windows_Version="7.8.0"
-Btm_Version="2.2.3"
+Linux_Version="8.0.1"
+Windows_Version="7.9.0"
+Btm_Version="2.2.5"
 
 FILES=(
 public/install/src/panel6.zip
